4233d9ccf8d524f0710d2c71281aa298c20e6200
robilkot <timur.robilko@gmail.com>
2024-11-17 00:13:55 +0300

    feat: implement saving to file

--- WeatherSensor/WeatherSensoeConsoleApp/Program.cs
+++ WeatherSensor/WeatherSensoeConsoleApp/Program.cs
@@ -2,7 +2,9 @@
 using PresentApp.Services;
 using WeatherSensoeConsoleApp;
 using WeatherSensorLib.Model;
+using WeatherSensorLib.Services;
 
+PersistenceService persistenceService = new();
 CommunicationService svc = new();
 
 Console.WriteLine("Available ports:");
@@ -11,6 +13,8 @@ svc.AvailablePortsNames.Each((port, index) =>
     Console.WriteLine($"{index}: {port}");
 });
 
+
+Console.WriteLine("Input port index:");
 var portIndex = int.Parse(Console.ReadLine()!);
 
 ConnectionParameters parameters = new(svc.AvailablePortsNames[portIndex]);
@@ -18,6 +22,11 @@ ConnectionParameters parameters = new(svc.AvailablePortsNames[portIndex]);
 svc.MessageReceived += (sender, message) =>
 {
     Console.WriteLine($"{message.Timestamp} {message.WindSpeedMean} {message.WindDirectionMean}");
+
+    if (persistenceService.TryAppendToFile(message, "output.json") is (false, Exception ex))
+    {
+        Console.WriteLine($"Exception while saving message: {ex.Message}");
+    }
 };
 
 svc.MessageCorrupted += (sender, args) =>
---
+++ WeatherSensor/WeatherSensorLib/Services/PersistenceService.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using System.Text.Json;
+using WeatherSensorLib.Model;
+
+namespace WeatherSensorLib.Services
+{
+    public class PersistenceService
+    {
+        private static readonly JsonSerializerOptions s_options = new()
+        {
+            WriteIndented = false,
+        };
+
+        public (bool, Exception?) TryAppendToFile(WeatherSensorMessage message, string filename)
+        {
+            try
+            {
+                using var reader = new FileStream(filename, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+                int offset = 2;
+                byte[] lastSymbols = new byte[offset];
+
+                // Making sure we don't look beyond the stream size
+                reader.Seek(-Math.Min(reader.Length, offset), SeekOrigin.End);
+                reader.Read(lastSymbols, 0, offset);
+
+                // If file already contains a list of messages
+                if (lastSymbols is [.., (byte)']'])
+                {
+                    // Move stream to last record in list
+                    reader.Seek(-offset, SeekOrigin.End);
+                    bool fileAlreadyContainsMessages = reader.ReadByte() == '}';
+
+                    var serializedMessage = JsonSerializer.Serialize(message, s_options);
+                    
+                    // Append comma
+                    if (fileAlreadyContainsMessages)
+                    {
+                        reader.Write([(byte)',']);
+                    }
+
+                    reader.Write(Encoding.UTF8.GetBytes(serializedMessage));
+
+                    // Append list enclosure because it's been overwritten by message
+                    reader.Write([(byte)']']);
+                }
+                // If the file is yet empty
+                else
+                {
+                    var serializedMessage = JsonSerializer.Serialize(new List<WeatherSensorMessage>() { message }, s_options);
+
+                    reader.Write(Encoding.UTF8.GetBytes(serializedMessage));
+                }
+            }
+            catch (Exception e)
+            {
+                return (false, e);
+            }
+
+            return (true, null);
+        }
+    }
+}
